a1e47662b2d5d691dee1c13fb371a0e5309296b6
Oved Axel Zuñiga <Al02670615@tecmilenio.mx>
2020-10-05 23:15:33 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Primo primo = new Primo(10);
+	    Fibonacci fibonacci = new Fibonacci (10);
+
+	    var primos = primo.getPrimos();
